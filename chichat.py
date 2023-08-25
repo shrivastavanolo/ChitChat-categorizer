7a83e8a4de11153da88c9b1a92b36e44e976e8a1
Shreya Shrivastava <126598622+shrivastavanolo@users.noreply.github.com>
2023-08-25 21:50:26 +0530

    Add files via upload

---
+++ chichat.py
@@ -0,0 +1,24 @@
+import openai
+from secret_key import Openai_key
+openai.api_key = Openai_key
+response = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {
+      "role": "system",
+      "content": "You will be provided with statements, and your task is to tell whether they are conversational chit-chat or not. Answer with 'it's chitchat' if the statement is chit-chat and answer with 'it's not chi-chat' if the statement is not chit-chat."
+    },
+    {
+      "role": "user",
+      "content": "Awesome."
+    },
+
+  ],
+  temperature=0,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+
+print(response.choices[0].message)
